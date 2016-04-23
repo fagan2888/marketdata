72df85281de0642ef4b71c3a6736a89a1391b2f1
Ubuntu <kumkeejjl@gmail.com>
2016-04-23 11:01:54 +0100

    rename CoinList, period constants

--- coinlist.py
+++ coinlist.py
@@ -3,7 +3,16 @@ from poloniex import Poloniex
 import pandas as pd
 
 
-class PoloCoinList():
+class CoinList():
+
+    FIVE_MINUTES = 60*5
+    FIFTEEN_MINUTES = FIVE_MINUTES * 3
+    HALF_HOUR = FIFTEEN_MINUTES * 2
+    HOUR = HALF_HOUR * 2
+    TWO_HOUR = HOUR * 2
+    FOUR_HOUR = HOUR * 4
+    DAY = HOUR * 24
+
     def __init__(self):
         self.polo = Poloniex()
         vol = self.polo.marketVolume()
